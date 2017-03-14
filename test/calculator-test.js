091c0d3c189c833d9a4afc88925b4ed29914ca7a
Wisbell <westleyisbell@gmail.com>
2017-03-14 10:30:21 -0500

    Completed first two tests on calculator.js dealing with the addition functionality

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,12 +1,29 @@
 // calculator should take in object and then call the necessary functions
 
 
-const { assert: { equal } } = require('chai')
+const { assert: { equal, include, isFunction } } = require('chai')
 const calc = require('../lib/calculator')
 
 describe('calculator', () => {
-  it('should run add function if operation is "+"', () => {
-    equal()
+  it('should check if operation has a valid operator', () => {
+    include('+-*/', calc.testObj.operator)
   })
+
+  it('should run the add function if operator is "+"', () => {
+    console.log(calc.add)
+    isFunction(calc.add, "is calc.add a function or not?")
+  })
+
+  // it('should run the subtract function if operator is "-"', () => {
+
+  // })
+
+  // it('should run the multiply function if operator is "*"', () => {
+
+  // })
+
+  // it('should run the divide function if operator is "/"', () => {
+
+  // })
 
 })
